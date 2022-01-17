82a23888c87b20f5c9dd81dae960e580e85466fc
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2022-01-17 21:01:28 +0530

    Added pattern 02 solution

---
+++ DSA with Kunal - Solutions/09-pattern-solution/Pattern02.java
@@ -0,0 +1,25 @@
+/*
+         *
+         **
+         ***
+         ****
+         *****
+
+ */
+
+public class Pattern02 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    public static void pattern(int n)
+    {
+        for(int row=1; row<=n; row++)
+        {
+            for(int col=1; col<=row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
