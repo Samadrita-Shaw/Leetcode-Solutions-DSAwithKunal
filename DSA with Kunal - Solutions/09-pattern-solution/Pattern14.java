d7918898fd219d84817d04da081e7e9eb5171b27
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2022-01-24 11:59:35 +0530

    Added pattern 14 solution

---
+++ DSA with Kunal - Solutions/09-pattern-solution/Pattern14.java
@@ -0,0 +1,30 @@
+/*   *********
+      *     *
+       *   *
+        * *
+         *
+*/
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    public static void pattern(int n)
+    {
+        for(int row=n; row>=1; row--)
+        {
+            for(int k=1; k<=n-row; k++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=1; col<=2*row-1; col++)
+            {
+                if( col==1 || row==n || col==2*row-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
