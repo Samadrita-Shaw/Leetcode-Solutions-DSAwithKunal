73a409d41b56afdfff72288ae62a758a10c5ac4a
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2022-01-27 01:22:13 +0530

    Added pattern 29 solution

---
+++ DSA with Kunal - Solutions/09-pattern-solution/Pattern29.java
@@ -0,0 +1,39 @@
+/*
+ *        *
+ **      **
+ ***    ***
+ ****  ****
+ **********
+ ****  ****
+ ***    ***
+ **      **
+ *        *
+
+ */
+
+public class Pattern29 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    public static void pattern(int n)
+    {
+        for(int row=1; row<=2*n-1; row++)
+        {
+            for(int col=1; col<=2*n; col++)
+            {
+                if(row==n)
+                    System.out.print("*");
+
+                else if(row<=n && col<=row || row<=n && col>2*n-row)
+                    System.out.print("*");
+
+                else if(row>n && col<=2*n-row || row>n && col>=row+1)
+                    System.out.print("*");
+
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
