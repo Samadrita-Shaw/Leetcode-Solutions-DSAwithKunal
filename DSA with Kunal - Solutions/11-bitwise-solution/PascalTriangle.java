13bab6ac129047468b0ee8c8d4e84ee8e66a157b
Samadrita Shaw <88196245+Samadrita-Shaw@users.noreply.github.com>
2022-03-02 08:54:04 +0530

    Added Pascal Triangle Solution

---
+++ DSA with Kunal - Solutions/11-bitwise-solution/PascalTriangle.java
@@ -0,0 +1,9 @@
+/*
+    Find the sum of nth row of the Pascal's Triangle.
+ */
+public class PascalTriangle {
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(1<<(n-1));
+    }
+}
